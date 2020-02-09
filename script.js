4f6d9678dfe03e9882083c3662baa51eac5632ee
Daniil <34283275+Romashka19@users.noreply.github.com>
2020-02-09 15:50:16 +0200

    git push

--- script.js
+++ script.js
@@ -1,58 +1,78 @@
-let key;
+let project_id;
 let projects = [];
-let task = [];
+let tasks = [];
 buildProject();
-check();
-function check() {
-    for(let i=0; i<localStorage.length; i++) {
-        key = localStorage.key(i);
-    }
+
+function createProject() {
+    let projectName = $('#projectName').val();
+    projects.push({name:projectName,task:tasks});
+    projects = JSON.stringify(projects);
+    console.log(projects);
+    localStorage.setItem('project',projects);
+    location.reload(true);
 }
 
 function buildProject(){
-    for(let i=0; i<localStorage.length;i++){
-        let prName = localStorage.getItem(key);
+    projects = JSON.parse(localStorage.getItem('project'));
+    for(let i=0; i<projects.length;i++){
         let projectContainer = document.getElementById('projectContainer');
         let projectLink = document.createElement('button');
-        projectLink.className = 'btn btn-primary';
-        projectLink.id = prName;
-        projectLink.innerHTML = prName;
+        projectLink.className = 'btn btn-primary btn-project';
+        projectLink.id = projects[i].name;
+        projectLink.innerHTML = projects[i].name;
         let br = document.createElement('br');
 
         projectContainer.appendChild(projectLink);
         projectContainer.appendChild(br);
     }
+    createEvent();
 }
 
-function createProject() {
-    let projectName = $('#projectName').val();
-    projects = [{name:projectName}];
-    localStorage.setItem(key,projects);
-    location.reload(true);
-}
-
-function buildTasks(){
-
-    for (let i = 0; i < localStorage.length; i++) {
-            let taskName = JSON.parse(localStorage.value(i));
-            let taskContainer = document.getElementById('taskContainer');
-            let taskBlock = document.createElement('div');
-            taskBlock.className = 'tasks';
-            //taskBlock.id =tryrt ;
-            let br = document.createElement('br');
-            let h3 = document.createElement('h3');
-            h3.innerHTML = taskName;
-
-            taskContainer.appendChild(taskBlock);
-            taskContainer.appendChild(h3);
-            taskContainer.appendChild(br);
+function createEvent() {
+    let btn = document.getElementsByClassName('btn-project');
+    for(let i=0;i<btn.length;i++){
+        btn[i].onclick = function(event){
+            project_id = btn[i].id;
+            buildTasks();
+            //console.log(project_id)
+        };
     }
 }
 
 function createTask() {
-
     let taskName = $('#taskName').val();
-    task = [{name:taskName}];
+    tasks.push({name:taskName});
+    for (let i=0;i<projects.length;i++){
+        if(project_id == projects[i].name) {
+            console.log(projects[i]);
+            projects[i].task = tasks;
+        }
+    }
+    projects = JSON.stringify(projects);
+    localStorage.setItem('project',projects);
     buildTasks();
 }
+
+function buildTasks(){
+    projects = JSON.parse(localStorage.getItem('project'));
+    for(let i=0; i<projects.length;i++){
+        if(project_id == projects[i].name) {
+            let getTasks = projects[i].task;
+            for (let j = 0; j < getTasks.length; j++) {
+                let taskContainer = document.getElementById('taskContainer');
+                let taskBlock = document.createElement('div');
+                taskBlock.className = 'tasks';
+                taskBlock.id = getTasks[j].name;
+                let br = document.createElement('br');
+                let h3 = document.createElement('h3');
+                h3.innerHTML = getTasks[j].name;
+
+                taskContainer.appendChild(taskBlock);
+                taskContainer.appendChild(h3);
+                taskContainer.appendChild(br);
+            }
+        }
+    }
+}
+
 
